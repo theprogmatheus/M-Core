55247b5bc5dd7c7ed9c27f438a1a1c858b61eafc
theprogmatheus <theprog.matheus@gmail.com>
2025-06-26 20:57:37 -0300

    Add velocity dependency

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -7,11 +7,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 }
 
 tasks.test {
