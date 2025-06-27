3d48a8bce36b7aa51a93d96fd50acfbbc97879d3
theprogmatheus <theprog.matheus@gmail.com>
2025-06-26 22:17:33 -0300

    Update MCoreVelocityPlugin

--- velocity/src/main/java/com/github/theprogmatheus/mc/mcore/velocity/MCoreVelocityPlugin.java
+++ velocity/src/main/java/com/github/theprogmatheus/mc/mcore/velocity/MCoreVelocityPlugin.java
@@ -1,5 +1,6 @@
 package com.github.theprogmatheus.mc.mcore.velocity;
 
+import com.github.theprogmatheus.mc.mcore.core.MCore;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
@@ -15,11 +16,20 @@ import com.velocitypowered.api.plugin.Plugin;
 )
 public class MCoreVelocityPlugin {
 
+    private final MCore mcore;
+
+    public MCoreVelocityPlugin() {
+        this.mcore = new MCore();
+        this.mcore.onLoad();
+    }
+
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        this.mcore.onEnable();
     }
 
     @Subscribe
     public void onProxyShutdownEvent(ProxyShutdownEvent event) {
+        this.mcore.onDisable();
     }
 }
