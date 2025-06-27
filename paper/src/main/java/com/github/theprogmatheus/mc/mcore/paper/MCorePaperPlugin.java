0f7b5de2afb106433bd86991179d560f0c7698fd
theprogmatheus <theprog.matheus@gmail.com>
2025-06-26 22:19:47 -0300

    Create paper plugin main class

---
+++ paper/src/main/java/com/github/theprogmatheus/mc/mcore/paper/MCorePaperPlugin.java
@@ -0,0 +1,28 @@
+package com.github.theprogmatheus.mc.mcore.paper;
+
+import com.github.theprogmatheus.mc.mcore.core.MCore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCorePaperPlugin extends JavaPlugin {
+
+    private final MCore mcore;
+
+    public MCorePaperPlugin() {
+        this.mcore = new MCore();
+    }
+
+    @Override
+    public void onLoad() {
+        this.mcore.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        this.mcore.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        this.mcore.onDisable();
+    }
+}
