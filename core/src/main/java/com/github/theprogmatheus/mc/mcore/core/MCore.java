d296fa9941953ce82871212d81d49718d11f7637
theprogmatheus <theprog.matheus@gmail.com>
2025-06-26 22:25:15 -0300

    Update MCore class

--- core/src/main/java/com/github/theprogmatheus/mc/mcore/core/MCore.java
+++ core/src/main/java/com/github/theprogmatheus/mc/mcore/core/MCore.java
@@ -1,9 +1,28 @@
 package com.github.theprogmatheus.mc.mcore.core;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 public class MCore {
 
-    public void onLoad(){}
-    public void onEnable(){}
-    public void onDisable(){}
+    private final Logger log;
+    private final File dataFolder;
+
+    public MCore(Logger log, File dataFolder) {
+        this.log = log;
+        this.dataFolder = dataFolder;
+    }
+
+    public void onLoad() {
+        this.log.info("M-Core loaded.");
+    }
+
+    public void onEnable() {
+        this.log.info("M-Core enabled.");
+    }
+
+    public void onDisable() {
+        this.log.info("M-Core disabled.");
+    }
 
 }
