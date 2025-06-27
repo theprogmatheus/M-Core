5e109479969a6d30d0146a503bfa1f2d944cfbf5
theprogmatheus <theprog.matheus@gmail.com>
2025-06-27 12:37:38 -0300

    Create register(), unregister() and getService() methods on ServiceManager

---
+++ core/src/main/java/com/github/theprogmatheus/mc/mcore/core/ServicesManager.java
@@ -0,0 +1,51 @@
+package com.github.theprogmatheus.mc.mcore.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ServicesManager {
+
+    private final Map<Class<?>, Object> services = new HashMap<>();
+
+    /**
+     * Registra um novo serviço.
+     *
+     * @param serviceClass - A classe chave do serviço que deve ser registrado.
+     * @param service      - O serviço que deve ser registrado.
+     * @param <S>          - Tipo do serviço que deve ser registrado.
+     * @return true se o serviço foi registrado com sucesso.
+     */
+    public <S> boolean register(Class<S> serviceClass, S service) {
+        if (serviceClass != null && service != null && serviceClass.isAssignableFrom(service.getClass())) {
+            this.services.put(serviceClass, service);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Remove um serviço registrado.
+     *
+     * @param serviceClass - A classe chave do serviço registrado.
+     * @return true se o serviço foi removido com sucesso.
+     */
+    public boolean unregister(Class<?> serviceClass) {
+        return this.services.remove(serviceClass) != null;
+    }
+
+    /**
+     * Resgata um serviço registrado anteriormente
+     *
+     * @param serviceClass - A classe chave do serviço registrado.
+     * @param <S>          - Tipó do serviço a ser resgatado.
+     * @return O serviço registrado ou null caso não for encontrado um serviço pela classe chave informada.
+     */
+    public <S> S getService(Class<S> serviceClass) {
+        Object service = this.services.get(serviceClass);
+        if (service != null && serviceClass.isAssignableFrom(service.getClass()))
+            return serviceClass.cast(service);
+
+        return null;
+    }
+
+}
